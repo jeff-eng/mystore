0555de3f11c3faf3b4e3e2328acd7cbf9e166a0f
JEFF ENG <ME@JEFFENG.COM>
2022-11-13 10:49:53 -0800

    Created Product Service with observable to return data stream from data.json. Subscribed to observable in Products component.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/Product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productDataURL = 'assets/data.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    // Assert return value from get method is array of Products; 
+    // this automatically maps returned response to an array of Products
+    return this.http.get<Product[]>(this.productDataURL);
+  }
+  
 }
