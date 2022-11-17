f0f78b1135ad1e8ff4be9172b42f1c8fd9106016
JEFF ENG <ME@JEFFENG.COM>
2022-11-16 18:25:57 -0800

    Added routes to routing module. Created components for header and page-not-found.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductThumbnailComponent } from './product-thumbnail/product-thumbnail.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     ProductThumbnailComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    NavHeaderComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
