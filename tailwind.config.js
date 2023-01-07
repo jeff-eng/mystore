e8e9036fe7b8caa4966acc3af605f7afdb951b15
JEFF ENG <ME@JEFFENG.COM>
2023-01-06 17:52:14 -0800

    Tweaked the product detail template CSS some more.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    './src/**/*.{html,ts,js}', './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('tw-elements/dist/plugin')
   ],
 }
